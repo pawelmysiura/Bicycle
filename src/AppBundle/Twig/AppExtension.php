21b8e0fe21bed97e304d4a879f0b2f055541c833
pawel <pawmysiura@gmail.com>
2018-03-13 12:01:27 +0100

    project

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -42,6 +42,18 @@ class AppExtension extends \Twig_Extension
                 [
                     'needs_environment' => true,
                     'is_safe' => ['html']
+                ]),
+            new \Twig_SimpleFunction('print_front_footer_menu', [
+                $this, 'printFrontFooterMenu'],
+                [
+                    'needs_environment' => true,
+                    'is_safe' => ['html']
+                ]),
+            new \Twig_SimpleFunction('print_panel_footer_menu', [
+                $this, 'printPanelFooterMenu'],
+                [
+                    'needs_environment' => true,
+                    'is_safe' => ['html']
                 ])
         ];
     }
@@ -96,6 +108,40 @@ class AppExtension extends \Twig_Extension
         try {
             return $environment->render('template/frontMenu.html.twig', [
                 'menu' => $adminMenu
+            ]);
+        } catch (\Twig_Error_Loader $e) {
+        } catch (\Twig_Error_Runtime $e) {
+        } catch (\Twig_Error_Syntax $e) {
+        }
+    }
+
+    public function printFrontFooterMenu(\Twig_Environment $environment)
+    {
+        $menu = [
+            $this->translator->trans('front.title.index',[] , 'controller') => 'front_index',
+            $this->translator->trans('front.title.about',[] , 'controller') => 'front_about',
+            $this->translator->trans('front.title.contact',[] , 'controller') => 'front_contact'
+        ];
+        try {
+            return $environment->render('template/frontFooterMenu.html.twig', [
+                'menu' => $menu
+            ]);
+        } catch (\Twig_Error_Loader $e) {
+        } catch (\Twig_Error_Runtime $e) {
+        } catch (\Twig_Error_Syntax $e) {
+        }
+    }
+
+    public function printPanelFooterMenu(\Twig_Environment $environment)
+    {
+        $menu = [
+            $this->translator->trans('panel.menu.post',[] , 'controller') => 'panel',
+            $this->translator->trans('panel.menu.bike_paths',[] , 'controller') => 'panel_maps',
+            $this->translator->trans('panel.menu.favourite',[] , 'controller') => 'panel_favourite_maps'
+        ];
+        try {
+            return $environment->render('template/frontFooterMenu.html.twig', [
+                'menu' => $menu
             ]);
         } catch (\Twig_Error_Loader $e) {
         } catch (\Twig_Error_Runtime $e) {
